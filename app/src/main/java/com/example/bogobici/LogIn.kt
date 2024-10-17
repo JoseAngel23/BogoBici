72257721d530edb01dd8b035d0bb8fffa870d636
Jose Manuel Forero Angel <josem.foreroa@utadeo.edu.co>
2024-10-17 16:03:43 -0500

    Agregando navegacion a primera pantalla

--- app/src/main/java/com/example/bogobici/LogIn.kt
+++ app/src/main/java/com/example/bogobici/LogIn.kt
@@ -1,7 +1,24 @@
 package com.example.bogobici
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class LogIn {
+class LogIn : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_log_in, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
+
