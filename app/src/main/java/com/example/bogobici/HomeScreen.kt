9a37d2d0c354584b94d8bce40c4258d9a1b5c3a5
Jose Forero Angel <josem.foreroa@utadeo.edu.co>
2024-10-31 18:50:55 -0500

    Agregando vectores

--- app/src/main/java/com/example/bogobici/HomeScreen.kt
+++ app/src/main/java/com/example/bogobici/HomeScreen.kt
@@ -6,17 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class ChooseSports : Fragment() {
+class HomeScreen : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choose_sports, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false)
     }
 }
