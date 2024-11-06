6f43a9f99a950a3b5d34f25e6a4c5c6d90f02bd1
Jose Forero Angel <josem.foreroa@utadeo.edu.co>
2024-11-06 15:13:04 -0500

    Antes de hacer merge

--- app/src/main/java/com/example/bogobici/ui/Auth/SignUp.kt
+++ app/src/main/java/com/example/bogobici/ui/Auth/SignUp.kt
@@ -21,6 +21,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class SignUp : Fragment() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,22 +32,47 @@ class SignUp : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
 
         val signUpButton = view.findViewById<Button>(R.id.register_bton)
-        val emailEditText = view.findViewById<EditText>(R.id.email_text)
-        val passwordEditText = view.findViewById<EditText>(R.id.editTextTextPassword)
+        val email = view.findViewById<EditText>(R.id.email_text)
+        val password = view.findViewById<EditText>(R.id.editTextTextPassword)
+        val confirmpassword = view.findViewById<EditText>(R.id.RetypePassword)
         val backButton = view.findViewById<ImageButton>(R.id.back_button_sign_up)
 
-        signUpButton.setOnClickListener {
+        backButton?.setOnClickListener {
             requireActivity().supportFragmentManager.commit{
-                findNavController().navigate(R.id.action_signUp_to_upload_photo)
+                findNavController().popBackStack()
             }
         }
 
-        backButton.setOnClickListener {
-            requireActivity().supportFragmentManager.commit{
-                findNavController().popBackStack()
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        signUpButton?.setOnClickListener {
+
+            val emailText = email?.text?.toString() ?: ""
+            val passwordText = password?.text?.toString() ?: ""
+            val confirmPasswordText = confirmpassword?.text?.toString() ?: ""
+
+            if (emailText.isNotEmpty() && passwordText.isNotEmpty() && confirmPasswordText.isNotEmpty()) {
+                if (passwordText == confirmPasswordText) {
+                    firebaseAuth.createUserWithEmailAndPassword(emailText, passwordText)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                            Toast.makeText(requireContext(), "Registro exitoso", Toast.LENGTH_SHORT).show()
+                            requireActivity().supportFragmentManager.commit{
+                                findNavController().navigate(R.id.action_signUp_to_upload_photo)
+                            }
+                        } else {
+                            Toast.makeText(requireContext(), "Error en el registro", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(requireContext(), "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
             }
         }
 
